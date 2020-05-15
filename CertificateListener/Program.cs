ec9e80a1a3810bac14bd5223d51f3cfa73c26a6e
mapoul <44639698+mapoul@users.noreply.github.com>
2020-05-15 14:39:30 +0200

    run once every 24 hours

--- CertificateListener/Program.cs
+++ CertificateListener/Program.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Runtime.ConstrainedExecution;
+using System.Threading;
 
 namespace CertificateListener
 {
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
+            
             MailService ms = new MailService();
-            /* Certificate certificate = new Certificate();
-             certificate.mUserUid = "SluEwNNVe6gjGmZD1Z3STvLelOa2";
-             FirebaseConnector fbConnector = new FirebaseConnector();
-             certificate = fbConnector.ReadCertificates().Result.Find(x => x.mExpirationDate.Contains("May 29, 2020"));
-             Console.WriteLine(certificate.mExpirationDate);
-             DateTime expirationDate = DateTime.Parse(certificate.mExpirationDate);
-             Console.WriteLine(expirationDate.ToString());
-             Console.WriteLine(fbConnector.getUserEmail(certificate).Result.mEmail);*/
-            ms.HandleUsersAndCertificates();
+            do
+            {
+                int startTime = DateTime.Now.Millisecond;
+                
+                ms.HandleUsersAndCertificates();
+                int endTime = DateTime.Now.Millisecond;
+                int sleepTime = 86400000 - (endTime - startTime);
+
+                Thread.Sleep(sleepTime);
+            } while (true);
         }
     }
 }
